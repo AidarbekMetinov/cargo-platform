a4d534f5e08b4ba42d355cac9d732e7d624c282c
Aidar <aidar.financier@gmail.com>
2022-12-02 22:00:45 +0600

    initial commit

---
+++ src/main/java/com/epam/cargoplatform/CargoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cargoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/cargoplatform/CargoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.cargoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
